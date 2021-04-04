10b04107174a214161dc092a0516b58a5ab1b3d8
Federico Stra <stra.federico@gmail.com>
2021-04-04 20:28:14 +0200

    Fix setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -1,21 +1,53 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
 
+with open("README.md") as fp:
+    long_description = fp.read()
+
 setup(
-    name='pdfshrink',
-    version='0.1.0',
-    description='`pdfshrink` takes a number of PDF files and tries to optimize them through a suitable call to `ghostscript`.',
-    long_description='Read the README',
-    author='Federico Stra',
-    author_email='stra.federico@gmail.com',
-    # packages=find_packages(),
-    py_modules=['pdfshrink'],
-    install_requires=[],
-    extras_require={
-        'test': ['coverage']
+    name="pdfshrink",
+    version="0.1.0",
+    description="`pdfshrink` takes a number of PDF files and tries to optimize them through a suitable call to `ghostscript`.",
+    license="GPLv3",
+    author="Federico Stra",
+    author_email="stra.federico@gmail.com",
+    url="https://github.com/FedericoStra/pdfshrink",
+    project_urls={
+        "Code": "https://github.com/FedericoStra/pdfshrink",
+        "Issue tracker": "https://github.com/FedericoStra/pdfshrink/issues",
     },
+    # packages=find_packages(),
+    py_modules=["pdfshrink"],
+    install_requires=[],
     entry_points={
-        'console_scripts': [
-            'pdfshrink=pdfshrink:main',
+        "console_scripts": [
+            "pdfshrink=pdfshrink:main",
         ],
     },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Office/Business",
+        "Topic :: System :: Archiving :: Compression",
+        "Topic :: Utilities",
+    ],
 )
